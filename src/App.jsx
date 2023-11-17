bc3483d7d089ef84e256d489f4676d6f06704943
Edward <116842980+Edward-Arth@users.noreply.github.com>
2023-11-17 18:00:07 -0500

    Ran vite, started header

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    
+  )
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+    return (
+        <div className="header-con">
+            <div className="name-title-links">
+                <div className="name">Edward Strahlendorf</div>
+                <div className="title">Junior Software Developer</div>
+                <div className="links"></div>
+            </div>
+            <div className="site-directory">
+                <div className="site-page"></div>
+                <div className="site-page"></div>
+                <div className="site-page"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
